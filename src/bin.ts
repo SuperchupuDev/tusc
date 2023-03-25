f9b7716561e065c6c647ce942afeb6498c5cbde9
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2023-03-25 13:54:05 +0000

    add api support for formatting, 0.3.1

--- src/bin.ts
+++ src/bin.ts
@@ -2,9 +2,19 @@
 import { argv, cwd } from 'node:process';
 import { run, update } from './index.js';
 
-const url = argv[2]?.replace(/https?:\/\//, '').replace('www.', '');
+const url = argv[2];
 const path = `${cwd()}/videos`;
 const openExplorer = !argv.includes('-e');
+
+if (!url.trim()) {
+  (async () => {
+    const { blue } = await import('yoctocolors');
+    console.log(
+      `Hi! Provide a URL to download a video from.\nYou can use ${blue('-e')} to stop explorer from opening on success`
+    );
+    process.exit(0);
+  })();
+}
 
 if (argv.includes('-u') || argv.includes('--update')) {
   update();
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,87 @@
-import { exec as _exec } from 'node:child_process';
+import { spawn as _spawn, type SpawnOptions } from 'node:child_process';
 import path from 'node:path';
 import { mkdir } from 'node:fs/promises';
-import { promisify } from 'node:util';
 
 export interface TuscOptions {
   url: string | null;
   path: string;
   openExplorer: boolean;
   ytDlpPath?: string;
+  extension?: Extension;
+  resolution?: number | 'best';
+  dataCallback?: (data: unknown) => unknown;
 }
 
-const exec = promisify(_exec);
+export type Extension = 'mp4' | 'mp3' | 'webm' | '3gp' | 'm4a' | 'ogg' | 'wav';
 
 export const defaultYtDlpPath = path.resolve('./yt-dlp.exe');
 
-export async function run({ url: _url, path, openExplorer, ytDlpPath = defaultYtDlpPath }: TuscOptions) {
-  const { blue, bold, red } = await import('yoctocolors');
+export const audioFormats = ['m4a', 'mp3', 'ogg', 'wav'];
+
+function getFormat(extension: Extension, resolution: number | 'best') {
+  const audioFormat = extension === 'mp4' ? 'm4a' : 'webm';
+  if (audioFormats.includes(extension)) {
+    return ['--extract-audio', '--audio-format', extension];
+  }
+
+  return resolution === 'best'
+    ? ['-f', `bv*[ext=${extension}]+ba[ext=${audioFormat}]/b[ext=${extension}] / bv*+ba/b`]
+    : [
+        '-f',
+        `bv*[height<=${resolution}][ext=${extension}]+ba[ext=${audioFormat}]/b[height<=${resolution}][ext=${extension}] / bv*+ba/b`
+      ];
+}
+
+async function spawn(
+  command: string,
+  args: readonly string[],
+  options: SpawnOptions,
+  dataCallback?: (data: unknown) => unknown
+) {
+  return new Promise((resolve, reject) => {
+    const child = _spawn(command, args, options);
+    child.stdout?.setEncoding('utf8');
+    child.stdout?.on('data', (data: string) => {
+      if (data.includes('ERROR:')) {
+        return reject(data);
+      }
+      dataCallback?.(data) ?? console.log(data);
+    });
+    child.stderr?.setEncoding('utf8');
+    child.stderr?.on('data', reject);
+    child.on('error', reject);
+    child.on('close', code => (code === 0 ? resolve(code) : reject(code)));
+  });
+}
+
+export async function run({
+  url: _url,
+  path,
+  openExplorer,
+  ytDlpPath = defaultYtDlpPath,
+  extension = 'mp4',
+  resolution = 1080,
+  dataCallback
+}: TuscOptions) {
   const url = _url?.replace(/https?:\/\//, '').replace('www.', '');
 
   if (!url) {
-    console.log(`Hi! Provide a URL to download a video from.\nYou can use ${blue('-e')} to stop explorer from opening on success`);
-    process.exit(0);
+    return null;
   }
 
-  const [domain] = url.split('.');
-  
-  if (['youtube', 'youtu'].includes(domain)) {
-    console.log(`Downloading ${bold(red('YouTube'))} video...`);
-    await mkdir(path, { recursive: true });
-    await exec(`${ytDlpPath} https://${url}`, { cwd: path }).then(({ stdout }) => console.log(stdout));
-  }
-  
-  if (['twitter', 't'].includes(domain)) {
-    console.log(`Downloading ${bold(blue('Twitter'))} video...`);
-    await mkdir(path, { recursive: true });
-    await exec(`${ytDlpPath} https://${url}`, { cwd: path }).then(({ stdout }) => console.log(stdout));
-  }
-  
+  console.log(`Downloading video...`);
+  await mkdir(path, { recursive: true });
+
+  const format = getFormat(extension, resolution);
+  await spawn(ytDlpPath, [`https://${url}`, ...format], { cwd: path, stdio: 'inherit' }, dataCallback);
+
   try {
-    if (openExplorer) await exec(`explorer "${path.replaceAll('/', '\\')}"`);
+    if (openExplorer) await spawn('explorer', [path.replaceAll('/', '\\')], { stdio: 'inherit' });
   } catch {}
 
+  return true;
 }
 
 export async function update(path: string = defaultYtDlpPath) {
-  return exec(`${path} -U`).then(({ stdout }) => console.log(stdout));
+  return spawn(path, ['-U'], { stdio: 'inherit' });
 }
